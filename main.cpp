714cf555843a387db68fc285a7962f2595407ff4
Mateusz <chmielek1924@gmail.com>
2024-07-27 00:47:19 +0200

    Created platform and rendering of the platform

--- main.cpp
+++ main.cpp
@@ -5,9 +5,24 @@
 #define FRAME_HEIGHT 500
 #define TITLE "Platform Game"
 
+
+sf::RectangleShape* configurePlatform(){
+    float width = 150.f,height=40.f;
+    sf::RectangleShape*s = new sf::RectangleShape(sf::Vector2f(150.f,30.f));
+    std::cout<<"Adddress s: "<<s<<std::endl;
+    float leftPos = (FRAME_WIDTH-width)/2;
+    float bottomPos = FRAME_HEIGHT - height;
+    s->setPosition(leftPos,bottomPos);
+    s->setFillColor(sf::Color::Green);
+    return s;
+}
 int main()
 {
+    sf::RectangleShape*platform=nullptr;
+    std::cout<<"Address: "<<platform<<std::endl;
     sf::RenderWindow window(sf::VideoMode(FRAME_WIDTH, FRAME_HEIGHT), TITLE);
+    platform = configurePlatform();
+    std::cout<<"Address: "<<platform<<std::endl;
     while (window.isOpen())
     {
         sf::Event event;
@@ -16,8 +31,9 @@ int main()
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         window.clear();
+        if(platform)
+            window.draw(*platform);
         window.display();
     }
 
